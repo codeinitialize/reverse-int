5dfd0398b3647c27dd215ad2d700e2c409d1ef7e
pavel <pavel.build@gmail.com>
2024-08-14 10:48:45 +0300

    feat: add solve

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  let nToStr = n.toString();
+
+  let reversedStr = nToStr.split('').reverse().join('');
+
+  let reversedNum = parseInt(reversedStr, 10);
+
+  return reversedNum;
 }
